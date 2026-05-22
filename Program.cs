652098266c493219186ee54be2ea7e874a5c0197
deanbunn <deanbunn@gmail.com>
2026-05-21 17:56:31 -0700

    20260521_1756hrs OAuth Token Get

--- Program.cs
+++ Program.cs
@@ -17,11 +17,24 @@ ucdAPIInfo.Test_ID = Environment.GetEnvironmentVariable("ROSETTA_TEST_ID");
 //Check for Required Client ID and Secret Before Making API Calls
 if(string.IsNullOrEmpty(ucdAPIInfo.Client_ID) == false && string.IsNullOrEmpty(ucdAPIInfo.Client_Secret) == false)
 {
-    //Initiate 
-    Console.WriteLine("Made it here!");
 
+    //Initiate Http Client to Get OAuth Token
+    using(var client = new HttpClient())
+    {
+        client.DefaultRequestHeaders.Add("client_id",ucdAPIInfo.Client_ID);
+        client.DefaultRequestHeaders.Add("client_secret",ucdAPIInfo.Client_Secret);
+        client.DefaultRequestHeaders.Add("grant_type","CLIENT_CREDENTIALS");
+        client.DefaultRequestHeaders.Add("scope","read:public");
 
+        HttpResponseMessage response = client.PostAsync(ucdAPIInfo.Token_Url, null).Result;
 
+        //Read Response Body
+        string responsebody = response.Content.ReadAsStringAsync().Result;
+
+        Console.WriteLine("Status: " + response.StatusCode);
+        Console.WriteLine(responsebody);
+
+    }
 
 
 
