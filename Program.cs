732cf3eba9302b66ec31eea1d183493af44a4ed5
deanbunn <deanbunn@gmail.com>
2026-05-21 17:25:37 -0700

    OAuth Header

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DotNetEnv;
+using RosettaAPIApp;
+
+//Load Environment Variables File
+Env.Load();
+
+//Initiate UCD API Info
+UCDAPIInfo ucdAPIInfo = new();
+
+//Load API Information
+ucdAPIInfo.Client_ID = Environment.GetEnvironmentVariable("ROSETTA_CLIENT_ID");
+ucdAPIInfo.Client_Secret = Environment.GetEnvironmentVariable("ROSETTA_CLIENT_SECRET");
+ucdAPIInfo.Base_Url = Environment.GetEnvironmentVariable("ROSETTA_BASE_URL");
+ucdAPIInfo.Token_Url = Environment.GetEnvironmentVariable("ROSETTA_OAUTH_URL");
+ucdAPIInfo.Test_ID = Environment.GetEnvironmentVariable("ROSETTA_TEST_ID");   
+
+//Check for Required Client ID and Secret Before Making API Calls
+if(string.IsNullOrEmpty(ucdAPIInfo.Client_ID) == false && string.IsNullOrEmpty(ucdAPIInfo.Client_Secret) == false)
+{
+    //Initiate 
+    Console.WriteLine("Made it here!");
+
+
+
+
+
+
+    
+}//End of Client ID and Client Secret Checks
+
---
+++ UCDAPIInfo.cs
@@ -0,0 +1,13 @@
+namespace RosettaAPIApp;
+
+public class UCDAPIInfo
+{
+    public string? Base_Url {get;set;}
+    public string? Token_Url {get;set;}
+    public string? Client_ID {get;set;}
+    public string? Client_Secret {get;set;}
+    public string? OAuth_Token {get;set;}
+    public string? Test_ID {get;set;}
+
+}
+
